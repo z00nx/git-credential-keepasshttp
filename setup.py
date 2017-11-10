4babcad12cd13a5e0ff1a6f10160b62fbf8e9e39
z00nx 0 <z00nx0@gmail.com>
2017-11-11 00:18:40 +1100

    Added exception handling when no operation is given, updated travis test, added requirements.txt and updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import sys
 from setuptools import setup
+install_requires = ["keepasshttp"]
+if sys.version_info[0:2] < (3, 0):
+    dependency_links = ["git+https://github.com/romankh/python-keepasshttp#egg=keepasshttp-0.1.0"]
+else:
+    dependency_links = ["git+https://github.com/MarkusFreitag/python3-keepasshttp#egg=keepasshttp-0.2.0"]
+
 setup(
     name='git-credential-keepasshttp',
     version='1.0',
@@ -15,5 +22,7 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
     keywords='git credential keepass keepasshttp',
-    scripts=['bin/git-credential-keepasshttp']
+    scripts=['bin/git-credential-keepasshttp'],
+    dependency_links=dependency_links,
+    install_requires=install_requires,
 )
